1a1b2b410c00e8bcfc541d30b5b58723ea74a7b3
GregFa <faragegr@gmail.com>
2023-08-09 07:49:12 -0500

    updated loco_example.jl

--- example/loco_example.jl
+++ example/loco_example.jl
@@ -1,2 +1,38 @@
 using BulkLMM
 using DataFrames
+
+########
+# Data #
+########
+bulklmmdir = dirname(pathof(BulkLMM));
+
+gmap_file = joinpath(bulklmmdir, "..", "data", "bxdData", "gmap.csv");
+gInfo = BulkLMM.CSV.read(gmap_file, BulkLMM.DataFrames.DataFrame);
+
+phenocovar_file = joinpath(bulklmmdir, "..", "data", "bxdData", "phenocovar.csv");
+pInfo = BulkLMM.CSV.read(phenocovar_file, BulkLMM.DataFrames.DataFrame);
+
+pheno_file = joinpath(bulklmmdir, "..", "data", "bxdData", "spleen-pheno-nomissing.csv");
+pheno = BulkLMM.DelimitedFiles.readdlm(pheno_file, ',', header = false);
+pheno_processed = pheno[2:end, 2:(end-1)] .* 1.0; # exclude the header, the first (transcript ID)and the last columns (sex)
+
+geno_file = joinpath(bulklmmdir, "..", "data", "bxdData", "spleen-bxd-genoprob.csv")
+geno = BulkLMM.DelimitedFiles.readdlm(geno_file, ',', header = false);
+geno_processed = geno[2:end, 1:2:end] .* 1.0;
+
+
+dfgeno = copy(gInfo)
+
+df_tmp = DataFrame(permutedims(geno_processed), :auto);
+insertcols!(df_tmp, 1, :Locus => gInfo.Locus)
+df_geno = leftjoin(gInfo, df_tmp, on = :Locus);
+
+############
+# Function #
+############
+gd = groupby(df_geno, :Chr)  
+
+combine(gd, nrow)
+combine(gd, x-> x[:,5:end])
+size(gd[1])
+map(x -> size(x, 1), gd)
