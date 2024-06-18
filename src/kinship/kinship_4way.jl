fd0018d727d329fa9a7135a2e8531e7fc81672f9
Durbadal0 <166731377+Durbadal0@users.noreply.github.com>
2024-06-18 16:04:23 -0500

    Added new files for functions

---
+++ src/kinship/kinship_4way.jl
@@ -0,0 +1,47 @@
+#The genotype data can be extracted from `cross object` using [r-qtl](https://rqtl.org/tutorials/) or [r-qtl2](https://kbroman.org/qtl2/assets/vignettes/user_guide.html).
+
+"""
+
+     kinship4way(genmat::Array{Float64,2})
+
+Computes a kinship for four-way cross data counting different alleles between two markers: ex. AB-AB=0; AB-AC=1; AB-CD=2,``\\dots``
+Note: In [R/qtl](https://cran.r-project.org/web/packages/qtl/qtl.pdf), genotypes are labeled as 1=AC; 2=BC; 3=AD; 4=BD by the function, `read.cross`.
+
+
+# Argument
+
+- `genmat` : A matrix of genotypes for `four-way cross` ``(1,2, \\dots)``.
+           size(genematrix)= (p,n), for `p` genetic markers x `n` individuals(or lines).
+
+# Output
+
+Returns a n x n symmetric matrix containing 1's on the diagonal.
+
+"""
+function kinship_4way(genmat::Array{Float64,2})
+    nmar, nind=axes(genmat)
+    kmat=zeros(nind,nind);
+    dist=zeros(nmar);
+
+    for i=nind
+        for j=i:length(nind)
+            for k=nmar
+                if (genmat[k,i]==genmat[k,j])
+                    dist[k]= 0.0
+                elseif (genmat[k,i]+genmat[k,j]==5)
+                    dist[k]=2.0
+                else
+                    dist[k]=1.0
+                end
+
+            end
+        kmat[j,i]=1.0-0.5*mean(dist)
+        kmat[i,j]=kmat[j,i]
+        end
+    end
+
+return kmat
+
+end
+
+
---
+++ src/kinship/kinship_lin.jl
@@ -0,0 +1,28 @@
+"""
+
+    kinshipLin(mat,cross)
+
+Calculates a kinship (or climatic relatedness, [`kinshipGs`](@ref)) matrix by linear kernel.
+
+# Arguments
+
+- `mat` : A matrix of genotype (or allele) probabilities usually extracted from [R/qtl](https://rqtl.org/tutorials/rqtltour.pdf),
+        [R/qtl2](https://kbroman.org/qtl2/assets/vignettes/user_guide.html), or the counterpart packages. size(mat)= (p,n) for p genetic markers x n individuals.
+- `cross` : A scalar indicating instances of alleles or genotypes in a genetic marker. ex. 1 for genotypes (labeled as 0,1,2), 2 for RIF, 4 for four-way cross, 8 for HS mouse (allele probabilities), etc.
+
+# Output
+
+Returns a n x n symmetric (positive definite) matrix containing 1's on the diagonal.
+
+See also [`kinshipCtr`](@ref), [`kinshipStd`](@ref) for genetype data.
+
+
+"""
+function kinship_lin(mat,cross)
+r=size(mat,1)/cross; n=size(mat,2)
+     K=Symmetric(BLAS.syrk('U','T',1.0,mat))/r
+   @views for j=1:n
+        K[j,j]=1.0
+          end
+    return convert(Array{Float64,2},K)
+end
