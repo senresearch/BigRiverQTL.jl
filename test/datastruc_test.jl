ce4d776f28df09ca2103d645d8b94d148751d945
Durbadal0 <166731377+Durbadal0@users.noreply.github.com>
2024-07-10 06:06:02 -0500

    Added tests for data structs

---
+++ test/datastruc_test.jl
@@ -0,0 +1,30 @@
+#############################
+# Generate Data for testing #
+#############################
+
+data_dir = joinpath(@__DIR__, "../data/BXD/")
+file = joinpath(data_dir, "bxd.json")
+
+
+
+# Checks whether the output of `get_bigriverqtldata` function is of type `BigRiverQTLData`
+function BigRiverQTLData_struct_test(filename::String, testname::String)
+    @testset "$testname" begin
+
+        println("Test results for checking whether the output of `get_bigriverqtldata` function is of type `BigRiverQTLData`: ", @test typeof(get_bigriverqtldata(filename))==BigRiverQTLData)
+         
+        
+    end
+        
+end
+
+
+
+
+#########################
+# Test: BigRiverQTLData #
+#########################
+
+
+
+BigRiverQTLData_struct_test(K1, "get_bigriverqtldata")
