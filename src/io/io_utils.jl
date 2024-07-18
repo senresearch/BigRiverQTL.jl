0f8f25eb1499557b1736b40540aac27278de87da
GregFa <faragegr@gmail.com>
2024-07-18 15:17:18 -0500

    added a function to load data from directory or json files

--- src/io/io_utils.jl
+++ src/io/io_utils.jl
@@ -40,7 +40,7 @@ Returns a data frame.
 
 
 """
-function read_data(filename)
+function read_data(filename::String)
 	# read the file into lines
 	lines = readlines(filename)
 	# which lines have # as first character
@@ -51,3 +51,22 @@ function read_data(filename)
 	return CSV.read(filename, DataFrame; header = startdata)#comment="#")
 end
 
+"""
+Need description
+"""
+function get_control_file(filename::String)
+
+    filename = realpath(filename)
+    if isfile(filename)
+        data_dir = dirname(realpath(filename))
+    elseif isdir(filename)
+        data_dir = filename
+        files_dir = readdir(data_dir) 
+        filename = files_dir |> 
+                    x ->  findall(occursin.(".json", filesdir)) |>
+                    x -> filesdir[x] |>
+                    x -> joinpath(data_dir, x) 
+    end
+
+	return data_dir, filename
+end
