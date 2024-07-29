cc7005c5df7c445757acad6396a81d092893ff0c
Durbadal0 <166731377+Durbadal0@users.noreply.github.com>
2024-07-29 16:24:26 -0500

    Updated plot_utils

--- src/plots/plots_utils.jl
+++ src/plots/plots_utils.jl
@@ -1,9 +1,9 @@
 function gmap2df(gmap::Gmap)
-    v=reduce(vcat,gInfo.marker_name);
+    vmarker=reduce(vcat,gInfo.marker_name);
     Mb=reduce(vcat,gInfo.pos);
     start=0
     vchr=repeat(["0"],sum(length.(gInfo.marker_name)))
-    for i in 1: eachindex()
+    for i in 1: eachindex(gInfo.chr)
         l_i=length(gInfo.marker_name[i])
         vchr[start+1:l_i+start] .= gInfo.chr[i]
         start=start+l_i
