9d453ab99b20dc9d8eaeec8432b5a0d3e0748752
Gregory Farage <faragegr@gmail.com>
2025-10-16 23:58:49 -0500

    Updated check_key description

--- src/io/io_helpers.jl
+++ src/io/io_helpers.jl
@@ -151,12 +151,12 @@ end
 
 
 """
-    check_key(control_dict::Dict, s::String) -> Any
+    check_key(control_dict::Union{Dict, JSON.Object}, s::String) -> Any
 
 Check if a specified key exists in the given dictionary and return its corresponding value.
 
 # Arguments
-* `control_dict::Dict`: A dictionary from which the value associated with a key is to be retrieved.
+* `control_dict::Union{Dict, JSON.Object}`: A dictionary from which the value associated with a key is to be retrieved.
 * `s::String`: The key for which the existence and value are checked within the dictionary.
 
 # Returns
